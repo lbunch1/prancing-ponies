a7c7f665d94729710251637791be843615c6231c
Logan Bunch <loganbunch@gmail.com>
2023-10-13 17:19:55 +0000

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'The Prancing Ponies';
+export const SITE_DESCRIPTION = 'The official website of the slightly delusional, aspiring cover band - The Prancing Ponies!';
